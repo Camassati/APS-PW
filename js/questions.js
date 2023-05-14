d2d1ddbf366c1c9d74d491cfecbb2ba57d357328
Camassati <gur4sp@gmail.com>
2023-05-14 17:03:29 -0300

    Aps unip

---
+++ js/questions.js
@@ -0,0 +1,82 @@
+export default [
+    {
+      question: "Qual das seguintes fontes de energia é considerada uma fonte de energia renovável?",
+      answers: [
+        { option: "Petróleo", correct: false},
+        { option: "Carvão", correct: false },
+        { option: "Energia solar", correct: true },
+      ],
+    },
+    {
+      question: "Qual é a principal vantagem da energia eólica em relação a outras fontes de energia?",
+      answers: [
+        { option: "É uma fonte de energia barata", correct: false },
+        { option: "É uma fonte de energia disponível o tempo todo", correct: false },
+        { option: "É uma fonte de energia limpa", correct: true },
+      ],
+    },
+    {
+      question: "Qual das seguintes fontes de energia não é considerada uma fonte de energia renovável?",
+      answers: [
+        { option: "Gás natural", correct: true },
+        { option: "Hidrelétrica", correct: false },
+        { option: "Geotérmica", correct: false },
+      ],
+    },
+    {
+      question: "O que é a energia geotérmica?",
+      answers: [
+        { option: "Energia produzida a partir do movimento das ondas do mar", correct: false },
+        { option: "Energia produzida a partir do calor interno da Terra", correct: true },
+        { option: "Energia produzida a partir da queima de combustíveis fósseis", correct: false },
+      ],
+    },
+    {
+      question: "Qual é a principal desvantagem da energia solar?",
+      answers: [
+        { option: "É uma fonte de energia cara", correct: true },
+        { option: "É uma fonte de energia poluente", correct: false },
+        { option: "É uma fonte de energia pouco eficiente", correct: false },
+      ],
+    },
+    {
+      question: "O que é a energia hidrelétrica?",
+      answers: [
+        { option: "Energia produzida a partir da queima de combustíveis fósseis", correct: false },
+        { option: "Energia produzida a partir do movimento das ondas do mar", correct: false },
+        { option: "Energia produzida a partir do movimento da água em represas", correct: true },
+      ],
+    },
+    {
+      question: "Qual é uma das vantagens dos carros elétricos para o meio ambiente?",
+      answers: [
+        { option: "Eles emitem menos gases de efeito estufa do que os carros a gasolina ou diesel", correct: true },
+        { option: "Eles produzem mais poluição sonora do que os carros convencionais", correct: false },
+        { option: "Eles são mais pesados e, portanto, causam mais desgaste nas estradas", correct: false },
+      ],
+    },
+    {
+      question: "Qual das seguintes fontes de energia é considerada a mais limpa?",
+      answers: [
+        { option: "Carvão", correct: false },
+        { option: "Petróleo", correct: false },
+        { option: "Energia solar", correct: true },
+      ],
+    },
+    {
+      question: "Qual é a principal vantagem da energia hidrelétrica em relação a outras fontes de energia?",
+      answers: [
+        { option: "É uma fonte de energia limpa", correct: false },
+        { option: "É uma fonte de energia barata", correct: false },
+        { option: "É uma fonte de energia disponível o tempo todo", correct: true },
+      ],
+    },
+    {
+      question: "Qual é a principal desvantagem da energia eólica?",
+      answers: [
+        { option: "É uma fonte de energia cara", correct: false },
+        { option: "É uma fonte de energia poluente", correct: false },
+        { option: "É uma fonte de energia intermitente", correct: true },
+      ],
+    },
+  ];
